c95146ce500f96f113b68c8a9affa581e58f3118
Santadd <hethere97@gmail.com>
2022-09-13 13:21:26 +0000

    Account confirmation tests

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,6 +1,7 @@
 import unittest
 from app import create_app, db
 from models import User
+import time
 
 class UserModelTestCase(unittest.TestCase):
     def setUp(self):
@@ -38,3 +39,31 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password='cat', username='abc', email='abc@email.com')
         u2 = User(password='cat', username='def', email='def@email.com')
         self.assertTrue(u.password_hash != u2.password_hash)
+        
+    # valid account confirmation token tests
+    def test_valid_confirmation_token(self):
+        u = User(password='cat', username='abc', email='abc@email.com')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token()
+        self.assertTrue(u.confirm_token(token))
+        
+    # Invalid account confirmation token tests
+    def test_invalid_confirmation_token(self):
+        u = User(password='cat', username='abc', email='abc@email.com')
+        u2 = User(password='cat', username='def', email='def@email.com')
+        db.session.add(u)
+        db.session.add(u2)
+        db.session.commit()
+        token = u.generate_confirmation_token()
+        self.assertFalse(u2.confirm_token(token))
+        
+    # Expired account confirmation token
+    def test_expired_confirmation_token(self):
+        u = User(password='cat', username='abc', email='abc@email.com')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token(expiration=1)
+        # sleep for 15 secs because of leeway of 10s in jwt
+        time.sleep(15)
+        self.assertFalse(u.confirm_token(token))
